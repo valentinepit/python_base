a34d77e2deb83f33884c90ea641207c545b3b8f6
Valentine <piterans.v@gmail.com>
2020-12-18 15:19:04 +0500

    delete 3

--- lesson_1_3.py
+++
@@ -1,7 +0,0 @@
-num = int(input('Please give me your number 0<n<10 \n')) #ввод изначального числа
-if num > 0 and num <10:
-    num_1 = num * 10 + num # получаем nn
-    num_2 = num * 100 + num_1 # получаем nnn
-    print(num + num_1 + num_2)           
-else:
-    print('Next time give me the RIGHT number')
