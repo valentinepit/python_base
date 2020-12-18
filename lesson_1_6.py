a2bb33505a89f65b14b1660a94970e4d03e200c2
Valentine <piterans.v@gmail.com>
2020-12-18 15:19:44 +0500

    delete 6

--- lesson_1_6.py
+++
@@ -1,9 +0,0 @@
-a = int(input('Please give me first day result \n')) #ввод показателей первого дня
-b = int(input('Please give me required result \n')) #ввод показателей искомого дня
-i = 1 # счетчик дней
-while a < b :
-    a = a*0.1 + a # увеличиваем a на 10%
-    i = i + 1 # увеличиваем счетчик дней
-print("You need to treining for ",i , " day(s)")    
-
-
