a6e60d44543eaf5ad9b9775cc8333e4d3aeb30db
Valentine <42617437+valentinepit@users.noreply.github.com>
2020-12-25 14:35:36 +0500

    Update Lesson_4_5.py

--- Lesson_4_5.py
+++ Lesson_4_5.py
@@ -10,7 +10,7 @@ from functools import reduce
 
 
 def my_func(prev_el, el):
-    return prev_el + el
+    return prev_el * el
 
 
 print(reduce(my_func, [i for i in range(100, 1001) if i % 2 == 0]))
